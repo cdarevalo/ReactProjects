eedd3e532f3f804a399fd8ce60f77a2ff3a554d1
Cristian David Arevalo <cdavid.arevalo@gmail.com>
2025-10-07 18:47:25 -0500

    Actualizar apellido

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>Mi nombre es Cristian David</code> and save to test HMR
+          Edit <code>Mi nombre es Cristian David Arevalo</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
